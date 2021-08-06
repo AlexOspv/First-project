c9a6a64e67f13a62d0c5a62cc5f0f60a4cae27b7
unknown <@5-36-comp2.Kometadom.local>
2021-08-06 14:37:47 +0300

    Login page test added

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_USERNAME = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
+    REGISTER_MAIL = (By.NAME, "registration-email")
+    REGISTER_PASSWORD1 = (By.NAME, "registration-password1")
+    REGISTER_PASSWORD2 = (By.NAME, "registration-password2")
---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+﻿from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "Wrong url, try again"
+         
+    def should_be_login_form(self):
+        login_username = self.is_element_present(*LoginPageLocators.LOGIN_USERNAME)
+        login_password = self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD)
+        assert login_username and login_password, "Login form is missing "
+        
+    def should_be_register_form(self):
+        register_mail = self.is_element_present(*LoginPageLocators.REGISTER_MAIL)
+        register_password1 = self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD1)
+        register_password2 = self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD2)
+        assert register_mail and register_password1 and register_password2, "Register form is missing "
