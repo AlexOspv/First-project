81fef9d105a7e7e31174ad3a9eccb6e904690010
unknown <@5-36-comp2.Kometadom.local>
2021-08-09 15:23:03 +0300

    Product page tset added

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators():
     REGISTER_MAIL = (By.NAME, "registration-email")
     REGISTER_PASSWORD1 = (By.NAME, "registration-password1")
     REGISTER_PASSWORD2 = (By.NAME, "registration-password2")
+
+class ProductPageLocators():
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    BOOK_NAME_MSG = (By.CSS_SELECTOR, ".alertinner > strong")
+    BOOK_PRICE_MSG = (By.CSS_SELECTOR, ".alertinner > p > strong")
+    
---
+++ pages/product_page.py
@@ -0,0 +1,36 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+import time
+
+class ProductPage(BasePage):
+    def add_book_to_basket(self):
+        ProductPage.should_be_add_to_basket_button(self)
+        ProductPage.click_add_to_basket_button(self)
+        ProductPage.solve_quiz_and_get_code(self)
+        time.sleep(5)
+        ProductPage.should_be_name_book_in_message(self)
+        ProductPage.should_be_value_price_in_message(self)
+
+    def value_price(self):
+        return self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN).text
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BTN), "'Add to basket button is missing"
+
+    def click_add_to_basket_button(self):
+        return self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN).click()
+
+    def should_be_name_book_in_message(self):
+        name_book = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        print(name_book)
+        name_book_message = self.browser.find_element(*ProductPageLocators.BOOK_NAME_MSG).text
+        print(name_book_message)
+        assert name_book == name_book_message, "Invalid book's name"
+
+    def should_be_value_price_in_message(self):
+        value_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        print(value_price)
+        value_price_message = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_MSG).text
+        print(value_price_message)
+        assert value_price == value_price_message, "Invalid basket's price"
