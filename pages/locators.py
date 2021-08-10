897c1ba25f5fcd1cabea3ae3247641f2d8a9b29e
unknown <@5-36-comp2.Kometadom.local>
2021-08-10 16:05:56 +0300

    added negative tests

--- pages/locators.py
+++ pages/locators.py
@@ -16,4 +16,8 @@ class ProductPageLocators():
     ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     BOOK_NAME_MSG = (By.CSS_SELECTOR, ".alertinner > strong")
     BOOK_PRICE_MSG = (By.CSS_SELECTOR, ".alertinner > p > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > .alert-success')
     
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -34,3 +34,11 @@ class ProductPage(BasePage):
         value_price_message = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_MSG).text
         print(value_price_message)
         assert value_price == value_price_message, "Invalid basket's price"
+   
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
