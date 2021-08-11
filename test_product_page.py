11e9b118ac17c051d80582660c48557a1d63271c
unknown <@5-36-comp2.Kometadom.local>
2021-08-11 13:56:56 +0300

    project ready

--- test_product_page.py
+++ test_product_page.py
@@ -71,7 +71,8 @@ class TestUserAddToBasketFromProductPage():
         product_page.open()
         product_page.click_add_to_basket_button()
         product_page.should_not_be_success_message()
-
+ 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/" 
         product_page = ProductPage(browser, link)
