d9e6cff17a5bc70d5270fffbafcc37b7a0f4bf1d
unknown <@5-36-comp2.Kometadom.local>
2021-08-11 14:19:09 +0300

    project ready

--- test_product_page.py
+++ test_product_page.py
@@ -5,8 +5,17 @@ from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/" 
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_book_to_basket() 
@@ -72,9 +81,9 @@ class TestUserAddToBasketFromProductPage():
         product_page.click_add_to_basket_button()
         product_page.should_not_be_success_message()
  
-    @pytest.mark.need_review
+    @pytest.mark.need_review1
     def test_user_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/" 
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019" 
         product_page = ProductPage(browser, link)
         product_page.open()
         product_page.add_book_to_basket() 
